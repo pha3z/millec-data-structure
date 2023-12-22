c6746e3ccc7112613db4caaedb98fdd79514eab3
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2023-12-22 18:12:41 +0800

    Added DeletedSlotsShouldNotBeAccessible() test case.

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using FluentAssertions;
 using MILLEC;
 
@@ -14,7 +15,7 @@ public class Tests
 
         const int ADD_COUNT = 2;
 
-        for (int i = 1; i <= ADD_COUNT; i++)
+        for (int i = 0; i < ADD_COUNT; i++)
         {
             millec.Add(i);
         }
@@ -24,5 +25,48 @@ public class Tests
         millec.RemoveAt(0);
         
         millec.ItemsCount.Should().Be(ADD_COUNT - 1);
+    }
+    
+    
+    [Test]
+    public void DeletedSlotsShouldNotBeAccessible()
+    {
+        var millec = new MILLEC<int>();
+
+        millec.ItemsCount.Should().Be(0);
+
+        const int ADD_COUNT = 5;
+
+        for (int i = 0; i < ADD_COUNT; i++)
+        {
+            millec.Add(i);
+        }
+        
+        // We are skipping first index
+        for (int i = 1; i < ADD_COUNT; i++)
+        {
+            millec.RemoveAt(i);
+        }
+        
+        Assert.DoesNotThrow(() =>
+        {
+            millec[0].Should().Be(0);
+        });
+        
+        for (int i = 1; i < ADD_COUNT; i++)
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                DoSomething(millec[i]);
+            });
+        }
+
+        return;
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        void DoSomething(int item)
+        {
+            
+        }
     }
 }
