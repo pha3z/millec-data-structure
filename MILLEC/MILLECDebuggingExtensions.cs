490fd138ca20c344d2b712d64e49da916ea1e63a
James <jhoux@jvxtechnology.com>
2023-12-23 22:07:15 -0600

    move extensions in another file

---
+++ MILLEC/MILLECDebuggingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MILLEC;
+
+public static class MILLECDebuggingExtensions
+{
+    public static T[] GetItemsArray<T>(ref this MILLEC<T> instance)
+    {
+        return instance._itemsArr;
+    }
+
+    public static byte[] GetBitVectorsArr<T>(ref this MILLEC<T> instance)
+    {
+        return instance._bitVecsArr;
+    }
+
+    public static int GetHighestTouchedIndex<T>(ref this MILLEC<T> instance)
+    {
+        return instance._highestTouchedIndex;
+    }
+}
